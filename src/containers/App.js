05f8a07e1bdf8b9865fcbec31c4183b1d9ad63b8
Robert Ris <take1risk@gmail.com>
2020-01-06 07:55:46 -0600

    add redux middleware logger

--- src/containers/App.js
+++ src/containers/App.js
@@ -31,7 +31,6 @@ class App extends Component {
 
   componentDidMount () {
   	console.log("store", this.props.store)
-  	// console.log("getState", this.props.store.getState())
   	fetch('https://jsonplaceholder.typicode.com/users')
 	  .then(response => response.json())
 	  .then(users => this.setState({robots: users}));
